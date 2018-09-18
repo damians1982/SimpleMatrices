dc350fa229091a187eb49e1caa93af4ccbfed25c
damian <poczta@damianstasiak.pl>
2018-09-18 10:12:58 +0200

    now in constructor we handle 2 dimensions matrix, and we have other version to show matrix -> method pokaz2

--- DSMatrix.rb
+++ DSMatrix.rb
@@ -6,8 +6,13 @@ class DSMatrix
 		@matrix = Array.new(x.to_i)
 		puts "Size: "+ @matrix.size.to_s
 		for i in 0..x-1 do
-			@matrix[i] = i
+			#binding.pry
+			@matrix[i] = Array.new(y.to_i)
 			puts "i = "+i.to_s
+			for j in 0..y-1 do
+				@matrix[i][j] = 0
+			end
+			#binding.pry
 		end
 		#binding.pry
 	end
@@ -16,16 +21,36 @@ class DSMatrix
 		puts "Printuje macierz"
 		local_var = 0
 		@matrix.length.times do
+			#binding.pry
 			puts @matrix[local_var]
 			local_var = local_var + 1
 		end
 	end
+
+	def pokaz2
+		puts"Printuje macierz(pokaz2)"
+		local_var = 0
+		for i in 0..@matrix.length do
+			#binding.pry
+			print @matrix[i]
+			#for j in 0..@matrix[i].length do
+			#	puts ""
+			#end
+			puts ""
+		end
+		puts @matrix.length
+	end
+
 	
 end
 
 
 puts "Pracujemy nad nasza DSMatrix"
 
-matrix = DSMatrix.new(4,3)
+matrix = DSMatrix.new(2,3)
 #binding.pry
 matrix.pokaz
+
+#print "Matrix.length = "
+#matrix.pokaz2
+
